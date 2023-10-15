3499795148618f1f587746c386dcba2533986779
aaagt <example_e_mail@github_space_man.com>
2023-10-15 21:09:53 +0500

    Init project

---
+++ src/main/java/aaagt/servlets/servlet/MainServlet.java
@@ -0,0 +1,54 @@
+package aaagt.servlets.servlet;
+
+import aaagt.servlets.controller.PostController;
+import aaagt.servlets.repository.PostRepository;
+import aaagt.servlets.service.PostService;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class MainServlet extends HttpServlet {
+    private PostController controller;
+
+    @Override
+    public void init() {
+        final var repository = new PostRepository();
+        final var service = new PostService(repository);
+        controller = new PostController(service);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        // если деплоились в root context, то достаточно этого
+        try {
+            final var path = req.getRequestURI();
+            final var method = req.getMethod();
+            // primitive routing
+            if (method.equals("GET") && path.equals("/api/posts")) {
+                controller.all(resp);
+                return;
+            }
+            if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
+                // easy way
+                final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+                controller.getById(id, resp);
+                return;
+            }
+            if (method.equals("POST") && path.equals("/api/posts")) {
+                controller.save(req.getReader(), resp);
+                return;
+            }
+            if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
+                // easy way
+                final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+                controller.removeById(id, resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
+
